083c7fb9f5862a7a09668e7b6a53ce2f5d4e4ca6
LuckyBelieve <luckybelieve10@gmail.com>
2023-08-14 22:16:36 +0200

    search bar updating

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../styles/home.css";
 import { Container, Row, Col } from "reactstrap";
 import Subtitle from "../shared/Subtitle";
-
+import SearchBar from "../shared/SearchBar";
 const Home = () => {
   return (
     <>
@@ -17,18 +17,32 @@ const Home = () => {
                 </div>
                 <h1>
                   Traveling opens the door to creating{" "}
-                  <span className="hightlight"> memories </span>
+                  <span className="highlight"> memories </span>
                 </h1>
                 <p>
                   Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                   Ducimus totam, quaerat dolor obcaecati omnis, alias
                   consectetur, magnam blanditiis officia unde hic aliquam qui
-                  placeat et debitis quia sequi. Ipsa fugiat maxime dolor
-                  accusantium asperiores, fuga reiciendis nam ullam officia
-                  laudantium.
+                  placeat et debitis quia sequi.
                 </p>
               </div>
             </Col>
+            <Col lg="2">
+              <div className="hero__img-box">
+                <img src="/images/hero-img01.jpg" alt="image" />
+              </div>
+            </Col>
+            <Col lg="2">
+              <div className="hero__img-box mt-4">
+                <video src="/images/hero-video.mp4" alt="video" controls/>
+              </div>
+            </Col>
+            <Col lg="2">
+              <div className="hero__img-box mt-5">
+                <img src="/images/hero-img02.jpg" alt="image" />
+              </div>
+            </Col>
+            <SearchBar/>
           </Row>
         </Container>
       </section>
