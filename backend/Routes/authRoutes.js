6831c41edba6c518bb922109f5fadc1f3196b23a
LuckyBelieve <ineza@heybuddy.co.in>
2024-02-12 08:33:00 +0200

    separated with backend

--- backend/Routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { Login, Register } from "../Controllers/authController.js";
-const Router = express.Router();
-
-Router.post("/register", Register);
-Router.post("/login", Login);
-
-export default Router;
